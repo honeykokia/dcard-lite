000a6a6b30a36843bf0badc271f1e6e8e0758cdb
honeykokia <honeykokia@gmail.com>
2026-01-13 13:49:12 +0800

    feat(error):新增ErrorMessage和Errorcode 統一錯誤訊息

---
+++ src/main/java/com/example/demo/common/error/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.example.demo.common.error;
+
+public interface ErrorCode {
+    String code();
+}
---
+++ src/main/java/com/example/demo/common/error/ErrorMessage.java
@@ -0,0 +1,5 @@
+package com.example.demo.common.error;
+
+public enum ErrorMessage {
+    VALIDATION_FAILED, UNAUTHORIZED, CONFLICT
+}
