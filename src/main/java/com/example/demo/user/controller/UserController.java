48a1f177d3588de66cc54089d88dfcf402cc9df2
honeykokia <honeykokia@gmail.com>
2026-01-15 18:20:21 +0800

    feat(user):新增PATH`POST /users/register`和`POST /users/login`

---
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.demo.user.controller;
+
+import com.example.demo.user.dto.LoginRequest;
+import com.example.demo.user.dto.LoginResponse;
+import com.example.demo.user.dto.RegisterUserRequest;
+import com.example.demo.user.dto.RegisterUserResponse;
+import com.example.demo.user.repository.UserRepository;
+import com.example.demo.user.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUserRequest registerUserRequest) {
+        RegisterUserResponse registerUserResponse = userService.registerUser(registerUserRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerUserResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest loginRequest) {
+        LoginResponse loginResponse = userService.loginUser(loginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
+    }
+}
