25279830f0a578c24c6d3da5389b58fd04127742
honeykokia <honeykokia@gmail.com>
2026-01-15 18:20:49 +0800

    feat(user):新增UserRepository

---
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.user.repository;
+
+import com.example.demo.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
