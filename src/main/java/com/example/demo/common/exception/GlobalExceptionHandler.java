0d3d0f5192d996c45c53eb53806e3a9e23bb9c97
honeykokia <honeykokia@gmail.com>
2026-01-18 21:18:49 +0800

    feat(exception):新增型別不一致判斷 (PARAM_FORMAT_ERROR)

--- src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -3,14 +3,18 @@ package com.example.demo.common.exception;
 import com.example.demo.common.error.ErrorMessage;
 import com.example.demo.common.error.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -35,17 +39,21 @@ public class GlobalExceptionHandler {
             MethodArgumentNotValidException ex, HttpServletRequest request) {
 
         // 1. 取得 @NotNull(message="NAME_INVALID") 裡面的字串
-        String codeStr = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        FieldError error = ex.getBindingResult().getFieldErrors().get(0);
+        String errorCode;
 
-        // 2. 如果 DTO 沒寫 message，給個預設值
-        if (codeStr == null) codeStr = "INVALID_PARAM";
+        if (error.isBindingFailure()){
+            errorCode = "PARAM_FORMAT_ERROR";
+        }else{
+            errorCode = error.getDefaultMessage();
+        }
 
-        // 3. 組裝你的固定 Response 格式
+        // 2. 組裝你的固定 Response 格式
         ErrorResponse reponse = new ErrorResponse(
                 400,
                 "Bad Request",
                 ErrorMessage.VALIDATION_FAILED, // 固定大類
-                codeStr,          // 動態細項
+                errorCode,          // 動態細項
                 request.getRequestURI(),
                 Instant.now()
         );
