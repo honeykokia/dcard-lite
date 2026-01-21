bdf13650a68fa913ce5cef9521bdfc3c2f3cc4d9
honeykokia <honeykokia@gmail.com>
2026-01-21 11:30:02 +0800

    feat(post):新增PostRepository、PostService、PostController，實作發表文章功能

---
+++ src/main/java/com/example/demo/post/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.post.repository;
+
+import com.example.demo.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
