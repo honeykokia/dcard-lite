5f450cc0eb7c86c37e0644e55dcc9861d17d35a2
honeykokia <honeykokia@gmail.com>
2026-01-26 15:37:07 +0800

    feat(post):新增PAGE_INVALID, PAGE_SIZE_INVALID

--- src/main/java/com/example/demo/post/error/PostErrorCode.java
+++ src/main/java/com/example/demo/post/error/PostErrorCode.java
@@ -8,7 +8,9 @@ public enum PostErrorCode implements ErrorCode {
     SECURITY_UNAUTHORIZED,
     PATH_FORMAT_ERROR,
     BODY_INVALID,
-    TITLE_INVALID;
+    TITLE_INVALID,
+    PAGE_INVALID,
+    PAGE_SIZE_INVALID;
 
 
 
