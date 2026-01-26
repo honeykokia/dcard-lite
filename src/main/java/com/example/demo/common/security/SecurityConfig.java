dc73879f6bf35021823a4e5feb3abb20c116120f
honeykokia <honeykokia@gmail.com>
2026-01-26 15:40:28 +0800

    feat(security):開放 `GET /boards/{boardId}/posts` 存取權限

--- src/main/java/com/example/demo/common/security/SecurityConfig.java
+++ src/main/java/com/example/demo/common/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/boards").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/boards/{boardId}/posts").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
