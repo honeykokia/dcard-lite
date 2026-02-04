bcf91fea795f13b0c79f320aa9679c2cca14ac55
honeykokia <honeykokia@gmail.com>
2026-02-04 16:53:44 +0800

    feat(error):主要錯誤訊息新增FORBIDDEN

--- src/main/java/com/example/demo/common/error/ErrorMessage.java
+++ src/main/java/com/example/demo/common/error/ErrorMessage.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED),
-    CONFLICT(HttpStatus.CONFLICT),
+    FORBIDDEN(HttpStatus.FORBIDDEN),
     NOT_FOUND(HttpStatus.NOT_FOUND),
+    CONFLICT(HttpStatus.CONFLICT),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final HttpStatus status;
