acd5cf23f9fdcc8206b8592c219ed9ec4933f131
honeykokia <72547838+honeykokia@users.noreply.github.com>
2026-02-15 14:13:55 +0800

    [ARCH] 重構前端專案並遷移Docs至根目錄 (#12)
    主要變更：
    * 後端文件路徑更新
        * backend/docs -> docs/
    * 前端專案更新
        * JS -> TS
        * 專案架構修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,11 @@
-<script setup></script>
+<script setup lang="ts"></script>
 
 <template>
-  <router-view></router-view>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-
-<style>
-@import "tailwindcss";
-
-</style>
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import { useUserStore } from './stores/user'
 
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia)
+app.use(createPinia())
 app.use(router)
-
-// 初始化 user store
-const userStore = useUserStore()
-userStore.init()
 
 app.mount('#app')
